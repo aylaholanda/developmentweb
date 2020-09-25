a75982b5f1493e9597274062874a2a5686adf148
Ayla de Holanda Monteiro <devholandam@gmail.com>
2020-09-25 19:54:34 -0300

    Updates

---
+++ precache-manifest.354936e02bc49a6f186720c17cf7567f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d968cd68a244ed757fe958f3354c83f4",
+    "url": "/developmentweb/src/app.js/index.html"
+  },
+  {
+    "revision": "8a1e681bcfee0da76621",
+    "url": "/developmentweb/src/app.js/static/css/main.a9095f30.chunk.css"
+  },
+  {
+    "revision": "214a7d86727d9dcb777e",
+    "url": "/developmentweb/src/app.js/static/js/2.977f0a4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/developmentweb/src/app.js/static/js/2.977f0a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a1e681bcfee0da76621",
+    "url": "/developmentweb/src/app.js/static/js/main.b5c9377d.chunk.js"
+  },
+  {
+    "revision": "97264606def1d0b53879",
+    "url": "/developmentweb/src/app.js/static/js/runtime-main.fe56a98e.js"
+  },
+  {
+    "revision": "df45547c2f60b9fca148a1e6210cbf17",
+    "url": "/developmentweb/src/app.js/static/media/Annepro2.df45547c.jpg"
+  },
+  {
+    "revision": "5aec4372ea5c979f9904c0da6720aa2e",
+    "url": "/developmentweb/src/app.js/static/media/keyboard.5aec4372.jpg"
+  },
+  {
+    "revision": "aaf34fe7f3ccc1b20db07331e8795525",
+    "url": "/developmentweb/src/app.js/static/media/left-arrow.aaf34fe7.svg"
+  },
+  {
+    "revision": "55b60674a818b49fcc1799dac3679ece",
+    "url": "/developmentweb/src/app.js/static/media/logo.55b60674.png"
+  },
+  {
+    "revision": "1d292928e8fb1296e074fb9eb060de23",
+    "url": "/developmentweb/src/app.js/static/media/right-arrow.1d292928.svg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/developmentweb/src/app.js/static/media/svg-1.55cfb481.svg"
+  }
+]);
