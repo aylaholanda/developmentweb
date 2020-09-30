80d66a9e4b221ce01d20ca2eebd678e837e1574d
Ayla de Holanda Monteiro <devholandam@gmail.com>
2020-09-30 20:17:46 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developmentweb/frontend/public/index.js/precache-manifest.4ad2dbb8986021920766b0f5417c0c1e.js"
+  "/developmentweb/precache-manifest.732744aa459606361ef3f4ba6e846b16.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developmentweb/frontend/public/index.js/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developmentweb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
